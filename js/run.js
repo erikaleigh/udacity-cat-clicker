3e70c11090beb6b2139b5d41e2405a8f3b37db35
Erika LeBlanc <erikalleblanc@gmail.com>
2017-02-09 14:51:55 -0500

    Update js counter

--- js/run.js
+++ js/run.js
@@ -1,5 +1,11 @@
-var elem = document.getElementById('kitten');
+var elem = document.getElementById('kittenOne');
+var clickCount = document.getElementById('clicks');
+
 elem.addEventListener('click', function(){
-  var clicks = documents.getElementById('clicks');
-  
-}
+
+      var num = Number(clicks.innerHTML);
+      var numClicks = num++; 
+
+  console.log(num);
+  clicks.innerHTML = numClicks;
+},false);
