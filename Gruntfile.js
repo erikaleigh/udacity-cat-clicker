6282c9fb1e99f441e9fb5f7d92e850345fba28c6
Erika LeBlanc <erikalleblanc@gmail.com>
2017-02-10 09:57:37 -0500

    Resize images with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 700,
+            height: 525,
+            suffix: 'sm',
+            quality: 30
+          }]
+        },
+
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png,jpeg}'],
+          cwd: 'img_src/',
+          dest: 'img/'
+        }]
+      }
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['img']
+        },
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['mkdir', 'responsive_images']);
+
+};
