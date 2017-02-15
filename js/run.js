a992d6eb9146b053e58ba9712895c3f3c07c4c31
Erika LeBlanc <erikalleblanc@gmail.com>
2017-02-14 21:46:48 -0500

    Add extended comments to clickListen function

--- js/run.js
+++ js/run.js
@@ -40,8 +40,8 @@ var appendCats = function() {
   appendCats();
 
 
-// event listener - counts the amount of clicks on a cat, returns new number
-
+/* event listener - counts the amount of clicks on a cat, returns new number, appends new number
+*/
 /* event listener function:
   - target "catImg" id with var elem
   - loop - for every (catImg) entry, do the following:
@@ -54,20 +54,22 @@ var appendCats = function() {
 */
 
 var clickListen = function() {
-  var elem = document.getElementsByClassName('catImg');
-  var clickCount = document.getElementsByClassName('clickCount');
+  var elem = document.getElementsByClassName('catImg');  /* Gets instances of the catImg class - event listener will be added to this class */
+  var clickCount = document.getElementsByClassName('clickCount'); /* Gets instances of the clickCount class - the innerHTML of this class will increase */
 
   for (i = 0; i < elem.length; i++) {
-    var num = clickCount[i].innerHTML;
+    var num = clickCount[i].innerHTML; /* Collects the number that is currently in the clickCount instance - it will increase by 1 when clicked */
     console.log(num); // logs the current number from clickCount
 
     elem[i].addEventListener('click', function(){
-          var updateNum = num++;
-          console.log(updateNum); /* logs the increasing number from clickCount when you click */
-          var catEntry = document.getElementsByClassName('cat');
-          var p = document.createElement('p');
-          // p.appendChild(updateNum);
-          // catEntry.appendChild(p);
+          var updateNum = num++; /* increases the number currently in the clickCount instance */
+          console.log(updateNum); /* logs the increasing number from clickCount instance when image is clicked */
+
+          var catEntry = document.getElementsByClassName('cat'); /* Gets instances of the 'cat' class - to append to this? How to refer to just the instance currently being used in the loop - update just the correct instance's number? */
+          console.log(catEntry); // logs the catEntry class instances
+
+          // add append here
+
       });
 
   }false;
